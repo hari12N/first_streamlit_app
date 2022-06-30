6b1b81d3eccdcb69869eda904da64b4340cc659c
Harin <108446633+hari12N@users.noreply.github.com>
2022-06-29 21:16:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.title('bluberry oatmeal')
-streamlit.title('kale and potates')
+streamlit.text('bluberry oatmeal')
+streamlit.text('kale and potates')
 
 
